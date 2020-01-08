8866bbb1d85ba758bf29f10d2af1dec8e9681058
Sean-Kim84 <sean.kim843@gmail.com>
2020-01-08 18:14:32 +0900

    6.Advanced Query

--- 4.user/index.js
+++ 4.user/index.js
@@ -17,6 +17,34 @@ const notes = [
   }
 ];
 
-document.querySelector('button').addEventListener('click', function(e){
-  console.log('Add a new todo...')
+// document.querySelector('button').addEventListener('click', function(e){
+//   console.log('Add a new todo...')
+
+// });
+
+// document.querySelectorAll('button')[1].addEventListener('click', function(){
+//   console.log('Delete All');   
+// })
+
+document.querySelector('#create-note').addEventListener('click', function(e){
+  e.target.textContent = "The button was clicked"
 });
+
+document.querySelector('#remove-all').addEventListener('click', function(e){
+  // e.target.textContent = "The button was removed"
+  document.querySelectorAll('.note').forEach(function(note){
+    note.remove();
+  })
+})
+
+
+/* Single */
+// p
+// #replace
+// .item
+
+/* Multiple */
+// p#order
+// button.inventory
+// h1#title.application
+// h1.application#title
