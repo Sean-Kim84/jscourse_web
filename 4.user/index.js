20b37cab265443e46c091e134bde4e1e62a9d346
Sean-Kim84 <sean.kim843@gmail.com>
2020-01-08 17:46:03 +0900

    5.Handling User Interaction

---
+++ 4.user/index.js
@@ -0,0 +1,22 @@
+const notes = [
+  {
+    title: "Lord of Ring",
+    body: "blahblahsslalal"
+  },
+  {
+    title: "Old man and Sea",
+    body: "blahblahsslalal"
+  },
+  {
+    title: "Wolfgang",
+    body: "blahblahsslalal"
+  },
+  {
+    title: "Son of God",
+    body: "blahblahsslalal"
+  }
+];
+
+document.querySelector('button').addEventListener('click', function(e){
+  console.log('Add a new todo...')
+});
